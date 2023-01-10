7dc59dbb016a6de362752b7a0d8f814691d49abb
bubatz <71530056+E8H@users.noreply.github.com>
2023-01-10 16:41:29 -0500

    Update event.js

--- js/event.js
+++ js/event.js
@@ -4,28 +4,22 @@ const body = document.getElementsByTagName('body');
 window.addEventListener("keydown", (event) => {
   if(event.ctrlKey && (event.key === "S" || event.key === "s")) {
      event.preventDefault();
-     body[0].innerHTML = "UZIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII CLUBBBBBBBBBBBBBBBBBB OWNNNNNNNNNNNNNNNNNNNNNSSSSSSSSSSSSSSSS YOUUUUUUUUUUUUU"
   }
 
   if(event.ctrlKey && (event.key === "C")) {
      event.preventDefault();
-     body[0].innerHTML = "UZIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII CLUBBBBBBBBBBBBBBBBBB OWNNNNNNNNNNNNNNNNNNNNNSSSSSSSSSSSSSSSS YOUUUUUUUUUUUUU"
   }
   if(event.ctrlKey && (event.key === "E" || event.key === "e")) {
      event.preventDefault();
-     body[0].innerHTML = "UZIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII CLUBBBBBBBBBBBBBBBBBB OWNNNNNNNNNNNNNNNNNNNNNSSSSSSSSSSSSSSSS YOUUUUUUUUUUUUU"
   }
   if(event.ctrlKey && (event.key === "I" || event.key === "i" || event.key === "Ä±")) {
      event.preventDefault();
-     body[0].innerHTML = "UZIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII CLUBBBBBBBBBBBBBBBBBB OWNNNNNNNNNNNNNNNNNNNNNSSSSSSSSSSSSSSSS YOUUUUUUUUUUUUU";
   }
   if(event.ctrlKey && (event.key === "K" || event.key === "k")) {
      event.preventDefault();
-     body[0].innerHTML = "UZIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII CLUBBBBBBBBBBBBBBBBBB OWNNNNNNNNNNNNNNNNNNNNNSSSSSSSSSSSSSSSS YOUUUUUUUUUUUUU";
   }
   if(event.ctrlKey && (event.key === "U" || event.key === "u")) {
      event.preventDefault();
-     body[0].innerHTML = "UZIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII CLUBBBBBBBBBBBBBBBBBB OWNNNNNNNNNNNNNNNNNNNNNSSSSSSSSSSSSSSSS YOUUUUUUUUUUUUU";
   }
 });
 // stop right click
